5e4ca0f075edff0061f1a3a120a605d4bd4ab843
Christopher Volkmar <christophervolkmar@gmail.com>
2026-03-25 13:05:21 -0700

    Reapply "Add academic, dining, commuter parking markers"
    This reverts commit 2eb3b8edf1e951d432b62f3f6ab5379a2c3bef06.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -55,4 +56,10 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.maps.compose) // {x-release-please-version}
+    // Optionally, you can include the Compose utils library for Clustering,
+    // Street View metadata checks, etc.
+    //implementation(libs.maps.compose.utils) // {x-release-please-version}
+    // Optionally, you can include the widgets library for ScaleBar, etc.
+    //implementation(libs.maps.compose.widgets) // {x-release-please-version}
 }
---
+++ app/src/main/java/com/example/cinet/Map.kt
@@ -0,0 +1,140 @@
+package com.example.cinet
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.rememberMarkerState
+import com.google.maps.android.compose.rememberUpdatedMarkerState
+
+enum class LocationCategory {
+    ACADEMIC, COMMUTER_PARKING, DINING, HOUSING
+}
+
+data class CampusLocation(
+    val name: String,
+    val coordinates: LatLng,
+    val category: LocationCategory
+)
+
+@Composable
+fun Map() {
+    val csuci = LatLng(34.1621, -119.0435)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(csuci, 10f)
+    }
+    //Bounds to prevent the user from leaving the campus
+    val csuciBounds = LatLngBounds(
+        LatLng(34.155, -119.055),
+        LatLng(34.168, -119.035)
+    )
+    val mapProperties by remember {
+        mutableStateOf(
+            MapProperties(
+                latLngBoundsForCameraTarget = csuciBounds,
+                //min prevents from zooming out too far
+                minZoomPreference = 14f,
+                maxZoomPreference = 20f
+            )
+        )
+    }
+
+    val academicLocations = listOf(
+        CampusLocation("Aliso Hall", LatLng(34.161033533317216, -119.04554291019717), LocationCategory.ACADEMIC),
+        CampusLocation("Arroyo Hall", LatLng(34.160362571139856, -119.0449651371382), LocationCategory.ACADEMIC),
+        CampusLocation("Bell Tower", LatLng(34.161033426860186, -119.04327405083124), LocationCategory.ACADEMIC),
+        CampusLocation("Bell Tower East", LatLng(34.1613627873466, -119.04193224072041), LocationCategory.ACADEMIC),
+        CampusLocation("Bell Tower West", LatLng(34.16077413754563, -119.04413838504449), LocationCategory.ACADEMIC),
+        CampusLocation("Broome Library", LatLng(34.16272379037882, -119.04085866409868), LocationCategory.ACADEMIC),
+        CampusLocation("Chaparral Hall", LatLng(34.1620975730738, -119.04571940179336), LocationCategory.ACADEMIC),
+        CampusLocation("Del Norte Hall", LatLng(34.16314332680665, -119.04409404414939), LocationCategory.ACADEMIC),
+        CampusLocation("El Dorado Hall", LatLng(34.16421806224397, -119.04713710663997), LocationCategory.ACADEMIC),
+        CampusLocation("Gateway Hall", LatLng(34.164917303238425, -119.04526653254618), LocationCategory.ACADEMIC),
+        CampusLocation("Ironwood Hall", LatLng(34.16278192070162, -119.04589286088351), LocationCategory.ACADEMIC),
+        CampusLocation("Lindero Hall", LatLng(34.15952716715866, -119.04147320447237), LocationCategory.ACADEMIC),
+        CampusLocation("Madera Hall", LatLng(34.16291249920953, -119.04407142302078), LocationCategory.ACADEMIC),
+        CampusLocation("Manzanita Hall", LatLng(34.162752145230215, -119.04505967422949), LocationCategory.ACADEMIC),
+        CampusLocation("Malibu Hall", LatLng(34.16127971034514, -119.04036363349768), LocationCategory.ACADEMIC),
+        CampusLocation("Marin Hall", LatLng(34.16449348416405, -119.04518033627755), LocationCategory.ACADEMIC),
+        CampusLocation("Modoc Hall", LatLng(34.164118027818034, -119.0483579800719), LocationCategory.ACADEMIC),
+        CampusLocation("Napa Hall", LatLng(34.163790888257665, -119.04540396658717), LocationCategory.ACADEMIC),
+        CampusLocation("Ojai Hall", LatLng(34.16165367514373, -119.04266024098733), LocationCategory.ACADEMIC),
+        CampusLocation("Placer Hall", LatLng(34.163271303102604, -119.04319384405241), LocationCategory.ACADEMIC),
+        CampusLocation("Sage Hall", LatLng(34.164091848406024, -119.04219128128422), LocationCategory.ACADEMIC),
+        CampusLocation("Sierra Hall", LatLng(34.16234093150404, -119.04430321122032), LocationCategory.ACADEMIC),
+        CampusLocation("Shasta Hall", LatLng(34.16457201602076, -119.04463593717513), LocationCategory.ACADEMIC),
+        CampusLocation("Solano Hall", LatLng(34.16334765267999, -119.04519807735882), LocationCategory.ACADEMIC),
+        CampusLocation("Topanga Hall", LatLng(34.16009541167767, -119.04166559195076), LocationCategory.ACADEMIC),
+        CampusLocation("Yuba Hall", LatLng(34.164022422925036, -119.04109646938987), LocationCategory.ACADEMIC)
+    )
+    val diningLocations = listOf(
+        CampusLocation("American Pie Company", LatLng(34.16321171837447, -119.03931197917322), LocationCategory.DINING),
+        CampusLocation("Ekho's Café", LatLng(34.16309792014503, -119.03946822433113), LocationCategory.DINING),
+        CampusLocation("Freudian Sip", LatLng(34.162524752647805, -119.04086278682107), LocationCategory.DINING),
+        CampusLocation("Islands Cafe", LatLng(34.160430282161435, -119.04160590722023), LocationCategory.DINING),
+        CampusLocation("Mom Wong Kitchen", LatLng(34.16281772412409, -119.0392259681462), LocationCategory.DINING),
+        CampusLocation("Sea Store Market", LatLng(34.16137041992328, -119.04409100603398), LocationCategory.DINING),
+        CampusLocation("Tortillas Grill & Cantina", LatLng(34.16304343294648, -119.03946353999095), LocationCategory.DINING)
+    )
+    val commuterParking = listOf(
+        CampusLocation("A1 Parking", LatLng(34.163600395901376, -119.04259463208324), LocationCategory.COMMUTER_PARKING),
+        CampusLocation("A2 Parking", LatLng(34.16420505882201, -119.04162581055532), LocationCategory.COMMUTER_PARKING),
+        CampusLocation("A3 Parking", LatLng(34.16662463372068, -119.04684159985), LocationCategory.COMMUTER_PARKING),
+        CampusLocation("A4 Parking", LatLng(34.16422642302123, -119.0466532417237), LocationCategory.COMMUTER_PARKING),
+        CampusLocation("A5 Parking", LatLng(34.160303180725336, -119.04458319659938), LocationCategory.COMMUTER_PARKING),
+        CampusLocation("A6 Parking", LatLng(34.16325112229069, -119.0421188239597), LocationCategory.COMMUTER_PARKING),
+        CampusLocation("A7 Parking", LatLng(34.16064329832965, -119.04108600595718), LocationCategory.COMMUTER_PARKING),
+        CampusLocation("A8 Parking", LatLng(34.16305514764397, -119.04031656504462), LocationCategory.COMMUTER_PARKING),
+        CampusLocation("A10 Parking", LatLng(34.15935028132672, -119.04041059647916), LocationCategory.COMMUTER_PARKING),
+        CampusLocation("A11 Parking", LatLng(34.164527100140525, -119.04799474404118), LocationCategory.COMMUTER_PARKING),
+    )
+    //Map of all campus markers
+
+    val campusRegistry: Map<LocationCategory, List<CampusLocation>> = mapOf(
+        LocationCategory.ACADEMIC to academicLocations,
+        LocationCategory.DINING to diningLocations,
+        LocationCategory.COMMUTER_PARKING to commuterParking
+    )
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        properties = mapProperties,
+        cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(LatLng(34.162, -119.043), 16f)
+        }
+    ) {
+        //flatten all markers into a single list to easily draw them onto the map
+        val markersToDraw = campusRegistry.values.flatten()
+        markersToDraw.forEach { location ->
+            // Create a specific state for each marker so we can control it
+            val markerState = rememberUpdatedMarkerState(position = location.coordinates)
+
+            Marker(
+                state = markerState,
+                title = location.name,
+                snippet = "Category: ${location.category.name.lowercase()}",
+                // Using different colors based on the enum keeps it readable
+                icon = BitmapDescriptorFactory.defaultMarker(
+                    when (location.category) {
+                        LocationCategory.ACADEMIC -> BitmapDescriptorFactory.HUE_RED
+                        LocationCategory.COMMUTER_PARKING -> BitmapDescriptorFactory.HUE_AZURE
+                        LocationCategory.DINING -> BitmapDescriptorFactory.HUE_ORANGE
+                        LocationCategory.HOUSING -> BitmapDescriptorFactory.HUE_VIOLET
+                    }
+                )
+            )
+        }
+    }
+}
